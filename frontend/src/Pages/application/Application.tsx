ffa70639b0c7abc15df0b6531b8b41f4277363bc
박민규 <gksktl111@naver.com>
2024-11-12 11:11:36 +0900

    feat: 헌옷수거 모달창 UI 작성

--- frontend/src/Pages/application/Application.tsx
+++ frontend/src/Pages/application/Application.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import laundryWomen from "../../Assets/Img/application/laundry_women.png";
 import premiumLaundry from "../../Assets/Img/application/premium_laundry.png";
 import dailyLaundry from "../../Assets/Img/application/daily_laundry.png";
@@ -8,6 +8,7 @@ import clothesCollectionBox from "../../Assets/Img/application/clothes_collectio
 import CustomServiceCard from "./CustomServiceCard";
 import ServiceCard from "./ServiceCard";
 import DailyLaundryModal from "./DailyLaundryModal";
+import OldClothesModal from "./OldClothesModal";
 
 const Custom_services = [
     {
@@ -49,15 +50,23 @@ const services = [
 ];
 
 const Application = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태를 관리
+    const [isDailyLaundryModal, setIsDailyLaundryModal] = useState(false);
+    const [isOldClothesModal, setIsOldClothesModal] = useState(false);
     const [selectedService, setSelectedService] = useState(null);
 
-    const openModal = () => {
-        setIsModalOpen(true);
+    const openDailyLaundryModal = () => {
+        setIsDailyLaundryModal(true);
+    };
+    const closeDailyLaundryModal = () => {
+        setIsDailyLaundryModal(false);
+        setSelectedService(null);
     };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
+    const openOldClothesModal = () => {
+        setIsOldClothesModal(true);
+    };
+    const closeOldClothesModal = () => {
+        setIsOldClothesModal(false);
         setSelectedService(null);
     };
 
@@ -87,22 +96,34 @@ const Application = () => {
                 </div>
 
                 <div className="col-span-1 grid grid-rows-2 gap-5">
-                    {services.map((service) => (
-                        <ServiceCard
-                            key={service.title}
-                            title={service.title}
-                            description={service.description}
-                            details={service.details}
-                            image={service.image}
-                            onClick={openModal}
-                        />
-                    ))}
+                    <ServiceCard
+                        key={services[0].title}
+                        title={services[0].title}
+                        description={services[0].description}
+                        details={services[0].details}
+                        image={services[0].image}
+                        onClick={openDailyLaundryModal}
+                    />
+                    <ServiceCard
+                        key={services[1].title}
+                        title={services[1].title}
+                        description={services[1].description}
+                        details={services[1].details}
+                        image={services[1].image}
+                        onClick={openOldClothesModal}
+                    />
                 </div>
 
-                {isModalOpen && (
+                {isDailyLaundryModal && (
                     <DailyLaundryModal
                         service={selectedService}
-                        closeModal={closeModal}
+                        closeModal={closeDailyLaundryModal}
+                    />
+                )}
+
+                {isOldClothesModal && (
+                    <OldClothesModal
+                        closeModal={closeOldClothesModal}
                     />
                 )}
             </div>
--- frontend/src/Pages/application/OldClothesModal.tsx
+++ frontend/src/Pages/application/OldClothesModal.tsx
@@ -1,23 +1,16 @@
 import React, {useState} from 'react';
-import emptyBasket from '../../Assets/Img/application/empty_laundry_basket.png';
-import fullBasket from '../../Assets/Img/application/full_laundry_basket.png';
+import clothes_collection_box from '../../Assets/Img/application/clothes_collection_box.png';
 
 // TODO: 타입 모듈화 하기
-const DailyLaundryModal = ({service, closeModal}: any) => {
+const OldClothesModal = ({closeModal}: any) => {
     const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
-    // 각 버튼 클릭 시 처리 함수
     const handleButton1Click = () => {
-        setSelectedOption("3.5KG 미만");
+        setSelectedOption("담기");
 
     };
 
-    const handleButton2Click = () => {
-        setSelectedOption("3.5KG 이상");
-
-    };
-
-    // 확인 버튼 클릭 시 처리 함수
+    // TODO: Api 나오면 연결
     const handleConfirmClick = () => {
         if (selectedOption) {
 
@@ -31,39 +24,27 @@ const DailyLaundryModal = ({service, closeModal}: any) => {
             onClick={closeModal}
         >
             <div
-                className="flex flex-col justify-between fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[550px] h-[500px] rounded-lg bg-white shadow-2xl p-5"
+                className="flex flex-col justify-between fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[350px] h-[500px] rounded-lg bg-white shadow-2xl p-5"
                 onClick={(event) => event.stopPropagation()} // 이벤트 전파 중지
             >
                 <h2 className="text-center text-xl font-bold mb-4">선택된 상품을 추가합니다</h2>
 
                 <div className="flex w-full h-full mb-4 gap-2">
                     <button
-                        className={`w-1/2 flex flex-col items-center justify-center p-2 border rounded-lg transition ${
-                            selectedOption === "3.5KG 미만"
+                        className={`w-full flex flex-col items-center justify-center p-2 border rounded-lg transition ${
+                            selectedOption === "담기"
                                 ? "border-blue-600 bg-blue-100"
                                 : "border-gray-300 hover:border-blue-600 hover:bg-blue-50"
                         }`}
                         onClick={handleButton1Click}
                     >
-                        <img src={emptyBasket} alt="Empty Basket" className="w-16 h-16 mb-2"/>
-                        <span className="text-black font-bold">3.5KG 미만</span>
-                        <span className="text-gray-400">3.5KG 까지 13,500원</span>
-                    </button>
-                    <button
-                        className={`w-1/2 flex flex-col items-center justify-center p-2 border rounded-lg transition ${
-                            selectedOption === "3.5KG 이상"
-                                ? "border-blue-600 bg-blue-100"
-                                : "border-gray-300 hover:border-blue-600 hover:bg-blue-50"
-                        }`}
-                        onClick={handleButton2Click}
-                    >
-                        <img src={fullBasket} alt="Full Basket" className="w-16 h-16 mb-2"/>
-                        <span className="text-black font-bold">3.5KG 이상</span>
-                        <span className="text-gray-400">초과 0.5KG당 1,500원 추가</span>
+                        <img src={clothes_collection_box} alt="Full Basket" className="w-16 h-16 mb-2"/>
+                        <span className="text-gray-400">
+                            다양한 옷들을 보내주세요! 하루세탁에서 검수후 기부를 진행할게요!
+                        </span>
                     </button>
                 </div>
 
-                {/* 하단 설명과 확인/닫기 버튼 */}
                 <div className="flex flex-col items-center justify-end">
                     <p className="text-center text-gray-500 mb-5 font-bold">
                         주문 목록에 추가하려면 확인 버튼을 눌러주세요.
@@ -76,13 +57,13 @@ const DailyLaundryModal = ({service, closeModal}: any) => {
                                     : "bg-blue-300 cursor-not-allowed"
                             }`}
                             onClick={handleConfirmClick}
-                            disabled={!selectedOption} // 옵션이 선택되지 않으면 비활성화
+                            disabled={!selectedOption}
                         >
                             확인
                         </button>
                         <button
                             className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 transition"
-                            onClick={closeModal} // 닫기 버튼 클릭 시 모달 닫기
+                            onClick={closeModal}
                         >
                             닫기
                         </button>
@@ -93,4 +74,4 @@ const DailyLaundryModal = ({service, closeModal}: any) => {
     );
 };
 
-export default DailyLaundryModal;
+export default OldClothesModal;
